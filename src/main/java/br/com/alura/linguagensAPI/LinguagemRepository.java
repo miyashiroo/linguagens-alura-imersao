7d8bfcddba2a39ca695ba3e45c6a50dc8388a9fc
miyashiroo <miyashiroo@gmail.com>
2022-07-25 06:10:31 -0400

    Commit inicial

---
+++ src/main/java/br/com/alura/linguagensAPI/LinguagemRepository.java
@@ -0,0 +1,7 @@
+package br.com.alura.linguagensAPI;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface LinguagemRepository extends MongoRepository<Linguagem,String>{
+    
+}
